9936d5e667c46017f463ac728c4d32541b54f106
Oskar Laing <oredgaming@gmail.com>
2026-08-13 11:15:12 -0400

    Updated ladle config

---
+++ .ladle/config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('@ladle/react').UserConfig} */
+export default {
+  // stories: "src/**/*.stories.{js,jsx,ts,tsx,mdx}",
+
+  outDir: "docs",
+  defaultStory: "welcome--welcome",
+  storyOrder: ["welcome--welcome", "*"]
+};
