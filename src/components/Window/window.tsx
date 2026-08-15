8c757ddfba2a82044fc3886506c47c419e2a8b7d
Oskar Laing <oredgaming@gmail.com>
2026-08-14 20:00:32 -0400

    Migrated window.tsx to emotion css

--- src/components/Window/window.tsx
+++ src/components/Window/window.tsx
@@ -1,41 +1,21 @@
-import { type ComponentProps, type ReactNode } from "react";
-import { cn } from "@/utils/cn";
-import { type ClassValue } from "clsx";
 import styled from "@emotion/styled";
-import { glow, inBorder, outBorder, palette, shadows } from "@/theme";
+import { inBorder, outBorder, palette, shadows } from "@/theme";
 
-const WindowHeaderText = ({
-  children,
-  className,
-  ...props
-}: {
-  children?: ReactNode;
-  className?: ClassValue;
-  props?: ComponentProps<"div">;
-}) => {
-  return (
-    <div className={cn("text-accent-heading text-xl shrink-0", className)}  {...props}>
-      {children}
-    </div>
-  );
-};
 
-const WindowHeader = ({
-  children,
-  className,
-  ...props
-}: {
-  children?: ReactNode;
-  className?: ClassValue;
-  props?: ComponentProps<"div">;
-}) => {
-  return (
-    <div className={cn("flex justify-between items-center h-7 gap-2", className)} {...props}>
-      {children}
-    </div>
-  )
-};
+const WindowHeaderText = styled.div`
+  color: ${palette.menu.heading.text};
+  font-size: 20px;
+  flex-shrink: 0;
+`;  
 
+
+const WindowHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 28px;
+  gap: 8px;
+`
 
 const WindowDrag = styled.div<{}>`
   background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAYAAAA8CAMAAABy8zuxAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAEhQTFRFIB8gISAhOTg5JCMkJSQlKCcoKSgpLCssLSwtODc4NzY3NjU1NTM0NDIyMjAxMS8vLy4vLSsrKigoKScnKygoKSYmLiwtLCoqWf/lzAAAAERJREFUeJzt0UkKgDAUBNGuxBgEEe9/UhGnfFtw7d5F8Q5QSNB6yOJoFLG9VLH+fAaTTDadKaY3NRjuCbOUCJaRM5jYLwWmND8cccluAAAAAElFTkSuQmCC");  
@@ -45,34 +25,16 @@ const WindowDrag = styled.div<{}>`
   height: 90%;
 `
 
-const WindowHeaderButton = ({
-  children,
-  className,
-  ...props
-}: {
-  children?: ReactNode;
-  className?: ClassValue;
-  props?: ComponentProps<"div">;
-}) => {
-  return (
-    <div
-      {...props}
-      className={cn("h-7 w-7 border-inset shrink-0", className)}
-    >
-      <button className="border-outset w-full h-full hover:bg-none hover:glow-button active:glow-button-inset" >
-        {children}
-      </button>
-
-    </div>
-  );
-};
-
-export const WindowHeaderButton2 = styled.button`
+const WindowHeaderButton = styled.button`
   ${inBorder}
   
   width: 28px;
   height: 28px;
   flex-shrink: 0;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   
   /* Box Shadow to create the Out effect without the outBorder */
   box-shadow: inset 3px 0px 3px -4px #221b18,inset -8px 0px 3px -8px #221b18,inset 0px 9px 2px -8px #e3e3e3,inset 0px 2px 2px 0px #282828,inset 0px -9px 3px -9px #221b18;
@@ -81,18 +43,34 @@ export const WindowHeaderButton2 = styled.button`
     background-color: ${palette.secondary.main};
     color: ${palette.secondary.contrastText};
     text-decoration: none;
-    box-shadow: ${shadows.orangeOutShadow};
-    filter: ${glow.orange};
+    box-shadow: inset 8px 0px 4px -8px #fff,
+                inset -8px 0px 4px -8px #fff,
+                inset 0px 9px 4px -8px #fff,
+                
+                inset 0px -9px 2px -8px #000,
+                inset 0px -9px 2px -8px #432400,
+                0px 0px 4px 0px #000,
+                inset 0px 0px 4px 2px #f9b44b;
+    filter: drop-shadow(0 0 3px ${palette.secondary.light})
   }
   &:active {
     background-color: ${palette.secondary.light};
     color: ${palette.secondary.contrastText};
     text-decoration: none;
-    box-shadow: inset 8px 0px 4px -8px #000,inset -8px 0px 4px -8px #000,inset 0px 9px 2px -8px #fff,inset 0px 8px 4px -8px #000,inset 0px -8px 4px -8px #000,inset 0px -9px 2px -8px var(--color-button-glow-dark) /* #432400 */,0px 0px 4px 0px #000,inset 0px 0px 4px 2px var(--color-glow-shadow) /* #f9b44b */;
+    box-shadow: inset 8px 0px 4px -8px #000,
+                inset -8px 0px 4px -8px #000,
+                inset 0px 9px 2px -8px #000,
+                inset 0px 8px 4px -8px #000,
+                inset 0px -8px 4px -8px #fff,
+                inset 0px -9px 2px -8px var(--color-button-glow-dark) /* #432400 */,
+                0px 0px 4px 0px #000,
+                inset 0px 0px 4px 2px var(--color-glow-shadow) /* #f9b44b */;
     filter: none;
   }
+  &:active > svg {
+    transform: translate(0px, 1px);
+  }
 `
-
 
 const WindowContent = styled.div<{}>`
   background-color: ${palette.menu.insetBackground};
@@ -100,7 +78,6 @@ const WindowContent = styled.div<{}>`
   ${inBorder}
   min-height: 20px;
 `;
-
 
 const Window = styled.div<{}>`
 
@@ -114,6 +91,5 @@ const Window = styled.div<{}>`
   ${outBorder}
 
 `
-
 
 export {Window, WindowContent, WindowDrag, WindowHeader, WindowHeaderText, WindowHeaderButton}
--- src/stories/window.stories.tsx
+++ src/stories/window.stories.tsx
@@ -1,42 +1,41 @@
-import { Window, WindowContent, WindowDrag, WindowHeader, WindowHeaderButton, WindowHeaderButton2, WindowHeaderText } from "@/components/Window/window"
-import "@/global.css"
+import {
+  Window,
+  WindowContent,
+  WindowDrag,
+  WindowHeader,
+  WindowHeaderButton,
+  WindowHeaderText,
+} from "@/components/Window/window";
+import "@/global.css";
 
 export const WindowStory = () => (
   <div className="flex flex-col gap-2">
     <Window>
       <WindowHeader>
-        <WindowHeaderText>
-          Settings
-        </WindowHeaderText>
+        <WindowHeaderText>Settings</WindowHeaderText>
 
-        <WindowDrag/>
+        <WindowDrag />
 
         <WindowHeaderButton>
-          X
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-search-icon lucide-search w-4 h-4"
+          >
+            <path d="m21 21-4.34-4.34" />
+            <circle cx="11" cy="11" r="8" />
+          </svg>
         </WindowHeaderButton>
       </WindowHeader>
 
-      <WindowContent>
-        Content
-      </WindowContent>
-    </Window>
-
-    <Window>
-      <WindowHeader>
-        <WindowHeaderText>
-          Settings
-        </WindowHeaderText>
-
-        <WindowDrag/>
-
-        <WindowHeaderButton2>
-          X
-        </WindowHeaderButton2>
-      </WindowHeader>
-
-      <WindowContent>
-        Content
-      </WindowContent>
+      <WindowContent>Content</WindowContent>
     </Window>
   </div>
 );
