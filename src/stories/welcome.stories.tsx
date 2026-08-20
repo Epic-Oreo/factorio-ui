baab8ba7d4b39c822e91dec686d3c6085e4c8c85
Oskar Laing <oredgaming@gmail.com>
2026-08-20 15:46:34 -0400

    - Updated welcome story to include syntax highlighted code examples. Still WIP

--- src/stories/welcome.stories.tsx
+++ src/stories/welcome.stories.tsx
@@ -1,7 +1,31 @@
 import "@/global.css";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+
+const firstString = `
+import { Button } from '@oskarlaing/factorio-ui';
+
+`
 
 export const Welcome = () => (
   <div>
     <h1 className="text-3xl">Factorio UI</h1>
+
+
+    <h3 className="mt-10 text-xl">To get started, install @oskarlaing/factorio-ui</h3>
+    <div className="rounded-lg overflow-hidden">
+      <SyntaxHighlighter language="shell" style={docco}>
+        yarn add @oskarlaing/factorio-ui
+      </SyntaxHighlighter>
+    </div>
+
+
+    <h3 className="mt-10 text-xl">Then implement your first component!</h3>
+    <div className="rounded-lg overflow-hidden">
+      <SyntaxHighlighter language="javascript" style={docco}>
+        {firstString}
+      </SyntaxHighlighter>
+    </div>
   </div>
 )
