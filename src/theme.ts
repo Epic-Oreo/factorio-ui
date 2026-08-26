cd4606f30437e7f6c7024a1aa5b9fdef0de4c14c
Oskar Laing <oredgaming@gmail.com>
2026-08-26 15:18:23 -0400

    New orange in shadow that better reflects real game

--- src/theme.ts
+++ src/theme.ts
@@ -7,7 +7,7 @@ const shadows = {
   grayOutShadow: "inset 8px 0px 4px -8px #000,inset -8px 0px 4px -8px #000,inset 0px 10px 2px -8px #e3e3e3,inset 0px 10px 2px -8px #282828,inset 0px -9px 2px -8px #000,0px 0px 4px 0px #000",
 
   orangeOutShadow: "inset 8px 0px 4px -8px #000,inset -8px 0px 4px -8px #000,inset 0px 9px 2px -8px #fff,inset 0px 8px 4px -8px #000,inset 0px -8px 4px -8px #000,inset 0px -9px 2px -8px #432400,0px 0px 4px 0px #000,inset 0px 0px 4px 2px #f9b44b",
-  orangeInShadow: "inset 0px 10px 2px -8px #000,inset 0px 9px 2px -8px #000,inset 8px 0px 4px -8px #563a10,inset 8px 0px 4px -8px #563a10,inset -8px 0px 4px -8px #563a10,inset -8px 0px 4px -8px #563a10,inset 0px 9px 2px -8px #563a10,inset 0px -9px 2px -8px #563a10,inset 0px -8.5px 0px -8px #563a10,0px 0px 4px 0px #000",
+  orangeInShadow: "inset 0px 10px 2px -8px #000,inset 0px 9px 2px -8px #000,inset 8px 0px 4px -8px #563a10,inset 8px 0px 4px -8px #563a10,inset -8px 0px 4px -8px #563a10,inset -8px 0px 4px -8px #563a10,inset 0px 9px 2px -8px #563a10,inset 0px -9px 2px -8px #a0a0a0,inset 0px -8.5px 0px -8px #563a10,0px 0px 4px 0px #000",
 
   greenOutShadow: "inset 8px 0px 4px -8px #000,inset -8px 0px 4px -8px #000,inset 0px 10px 2px -8px #95df99,inset 0px 10px 2px -8px #163218,inset 0px -9px 2px -8px #000,0px 0px 4px 0px #000",
   greenInShadow: "inset 0px 10px 2px -8px #000,inset 0px 9px 2px -8px #000,inset 8px 0px 4px -8px #3f5024,inset 8px 0px 4px -8px #3f5024,inset -8px 0px 4px -8px #3f5024,inset -8px 0px 4px -8px #3f5024,inset 0px 9px 2px -8px #3f5024,inset 0px -9px 2px -8px #3f5024,inset 0px -8.5px 0px -8px #3f5024,0px 0px 4px 0px #000",
