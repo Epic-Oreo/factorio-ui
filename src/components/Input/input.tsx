baaf59c0c5a540cac6a4b356b0d4bec75e0fb18a
Oskar Laing <oredgaming@gmail.com>
2026-08-31 20:52:33 -0400

    style: styling for input component

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,9 +1,24 @@
+import { elementResetStyle, palette } from "@/theme";
 import styled from "@emotion/styled";
 
 
 
 export const Input = styled.input`
+  ${elementResetStyle.buttonAndInput};
 
-  
 
+  background-color: ${palette.primary.main};
+  border-radius: 4px;
+  box-shadow: inset 8px 0px 4px -8px #000,inset -8px 0px 4px -8px #000,inset 0px -10px 2px -8px #e3e3e3,inset 0px -10px 2px -8px #282828,inset 0px 9px 1px -8px #000, inset 0px 9px 3px -8px #000;
+  height: 32px;
+  padding: 3px 10px;
+  font-weight: 500;
+
+  &:focus {
+    background-color: #ecdbb8;
+    box-shadow: inset 8px 0px 4px -8px #e39827,inset -8px 0px 4px -8px #e39827,inset 0px -10px 2px -8px #e3e3e3,inset 0px 9px 1px -8px #000;
+
+
+    /* box-shadow: */
+  }
 `;
